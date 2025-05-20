76a4088e282aaff95761ed4718e8774b99178e8f
Li Lin <guru.lin@gmail.com>
2025-05-20 12:04:23 +0800

    rename web subproject to api

--- api/src/main/java/net/vino9/vino/demo/TransferAPIApplication.java
+++ api/src/main/java/net/vino9/vino/demo/TransferAPIApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TransferAPIApplication {
     public static void main(String[] args) {
-        System.setProperty("spring.cloud.function.definition", "submit;result");
         SpringApplication.run(TransferAPIApplication.class, args);
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "vino-transfer"
 include(
     ":function",
-    ":web",
+    ":api",
     ":stream",
 )
 
