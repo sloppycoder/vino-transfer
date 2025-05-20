687f685e8779b27d7ba5e6f4eb4732834e2becdc
Li Lin <guru.lin@gmail.com>
2025-05-20 11:36:22 +0800

    fixed indentation error in application.yml

--- stream/src/main/java/net/vino9/vino/demo/TransferProcessorApplication.java
+++ stream/src/main/java/net/vino9/vino/demo/TransferProcessorApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TransferProcessorApplication {
     public static void main(String[] args) {
-        System.setProperty("spring.cloud.function.definition", "process");
         org.springframework.boot.SpringApplication.run(TransferProcessorApplication.class, args);
     }
 }
