86e80b23041c4eb9ed76dfeb2fbdc19220174265
Li Lin <guru.lin@gmail.com>
2025-05-20 13:02:19 +0800

    added error handler in api module

---
+++ api/src/main/java/net/vino9/vino/demo/ErrorHandler.java
@@ -0,0 +1,15 @@
+package net.vino9.vino.demo;
+
+import net.vino9.vino.demo.biz.exception.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> handleValidationException(ValidationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
--- api/src/test/java/net/vino9/vino/demo/TransferAPITests.java
+++ api/src/test/java/net/vino9/vino/demo/TransferAPITests.java
@@ -1,17 +1,19 @@
 package net.vino9.vino.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
 import net.vino9.vino.demo.biz.model.Transfer;
 import net.vino9.vino.demo.biz.model.TransferRequest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -35,7 +37,28 @@ class TransferAPITests {
         ResponseEntity<Transfer> response =
                 restTemplate.postForEntity("/submit,process,result", request, Transfer.class);
 
-        Assertions.assertEquals(200, response.getStatusCodeValue());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
         assertNotNull(response.getBody().getRefId());
+    }
+
+    @Test
+    void testTransferBadAccount() {
+        // fromAccount 1111 will always return error
+        TransferRequest request =
+                TransferRequest.builder()
+                        .fromAccount("1111")
+                        .currency("USD")
+                        .amount(new BigDecimal("100"))
+                        .toAccount("4321")
+                        .memo("test transfer")
+                        .build();
+
+        ResponseEntity<String> response =
+                restTemplate.postForEntity("/submit,process,result", request, String.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertTrue(response.getBody().contains("Invalid account"));
     }
 }
